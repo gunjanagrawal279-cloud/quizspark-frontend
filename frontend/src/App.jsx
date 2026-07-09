5d6e9e44e81ea8a73c9c91159c3991d58c0f7129
Gunjan Agrawal <gunjanagrawal279@gmail.com>
2026-07-09 10:06:33 +0530

    fix leaderboard import case

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
-import Leaderboard from "./pages/Leaderboard";
+import Leaderboard from "./pages/leaderboard";
 import About from "./pages/About";
 
 function App() {
